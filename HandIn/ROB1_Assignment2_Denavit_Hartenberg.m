9d49c83dd809a6bdd99610d0eca87adf8287ec2f
mortenjesper <mortenjesper@hotmail.com>
2019-09-16 14:45:44 +0200

    sim

--- HandIn/ROB1_Assignment2_Denavit_Hartenberg.m
+++ HandIn/ROB1_Assignment2_Denavit_Hartenberg.m
@@ -32,7 +32,7 @@ T= A(:,:,1)*A(:,:,2)*A(:,:,3)*A(:,:,4)*A(:,:,5)
 % Extract the DH parameters from the model
 
 % Defines transition to next coordinate. Rotated pi/2 for elbow joint
-L1 = Revolute('d', base_shoulder, 'alpha', pi/2);
+L1 = Revolute('d', base_shoulder, 'alpha', 0);
 L2 = Revolute('d', shoulder_elb, 'alpha', pi/2);
 L3 = Revolute('a', elb1_elb2, 'alpha', 0);
 L4 = Revolute('a', elb2_wri, 'alpha', -pi/2);
@@ -42,8 +42,9 @@ robot = SerialLink([L1, L2, L3, L4, L5]);
 
 %% Calculate the full transformation ??0 between base and end effector frames
 
-%robot.teach()
-%robot.fkine([0.1, 0.2])
+theta = [0 0 0 0 0];
+robot.plot(theta);
+robot.teach()
 
 %syms q1 q2 q3 q4 q5 q6
 
